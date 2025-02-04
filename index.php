3bcfb0b93df9084ecc4ec82cb048c7d31bfa8aa5
Septian Adi Pratama <septianadip7998@gmail.com>
2025-02-04 21:18:27 +0700

    crud works

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ $result = mysqli_query($conn, "SELECT * FROM engineer");
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Data Gaji Engineer</title>
-  <!-- Menggunakan CDN Tailwind CSS -->
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gradient-to-r from-blue-100 to-purple-100 min-h-screen">
